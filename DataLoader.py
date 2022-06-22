fbae4940155310600193c3bd29d01a964a49a4a5
phzehetn <philipp.zehetner@cern.ch>
2022-06-22 13:31:52 +0200

    Add X_ID, concatenate vertices, use edge definition to create one big graph, where that only connects LCs belonging to the same trackster

--- DataLoader.py
+++ DataLoader.py
@@ -31,12 +31,13 @@ class TracksterLoader(Dataset):
         self.strides = [0]
         self.root = root
         self.regex = regex
+        self.N_events = N_events
 
     # Maybe should be called __len__
     # https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
     def len(self):
         # return 5000 * len(self.root) # is this uniform over all files?
-        return 10
+        return self.N_events
 
     @property
     def raw_file_names(self):
@@ -52,10 +53,9 @@ class TracksterLoader(Dataset):
         starts = []
         stops = []
         N_tracksters = df.NTracksters.median().astype(int)
+        N_clusters = []
         for t in range(N_tracksters):
             trackster = df.loc[t].reset_index()
-            trackster_starts = []
-            trackster_stops = []
             df = df[cluster_indices]
             z_values = df.loc[t]['vertices_z'].unique()
             for i, z in enumerate(z_values):
@@ -66,31 +66,39 @@ class TracksterLoader(Dataset):
                 for l in layer:
                     for k in layer:
                         if l < k:
-                            trackster_starts.append(l.astype(int))
-                            trackster_stops.append(k.astype(int))
+                        # Connect nodes from the same layer (l < k makes sure connections aren't added twice)
+                        # Adding the sum of N_clusters assures that our graph is not connecting nodes belonging to different tracksters
+                            starts.append(int(np.sum(N_clusters)) + l.astype(int))
+                            stops.append(int(np.sum(N_clusters)) + k.astype(int))
                     for k in pre_layer:
-                        trackster_starts.append(l.astype(int))
-                        trackster_stops.append(k.astype(int))
-            starts.append(trackster_starts)
-            stops.append(trackster_stops)
+                        # Connect nodes from the previous layer
+                        # Adding the sum of N_clusters assures that our graph is not connecting nodes belonging to different tracksters
+                        starts.append(int(np.sum(N_clusters)) + l.astype(int))
+                        stops.append(int(np.sum(N_clusters)) + k.astype(int))
+            # It's important to only append this AFTER creating the edges for this trackster otherwise it will screw up the indices of the edges
+            N_clusters.append(len(trackster))
         return starts, stops
 
 
     def turn_df_to_graph(self, df):
+        """
+        df has to be a graph including a single event, it should have two indices, 
+        the first one belonging to the trackster and the second one to the cluster
+        """
         cluster_indices = ['vertices_x', 'vertices_y', 'vertices_z', 'vertices_energy']
-        graphs = []
         N_tracksters = df.NTracksters.median().astype(int)
-        all_edges = self.calculate_edges(df)
+        event_edges = self.calculate_edges(df)
+        X_ID = []
+        # y = calo energy
+        vertices = torch.from_numpy(df[cluster_indices].values)
         for i in range(N_tracksters):
-            trackster = df.loc[i]
-            vertices = trackster[cluster_indices]
-            vertices = vertices.values
-            vertices = vertices.astype(np.float32)
-            vertices = torch.from_numpy(vertices)
-            edges = (all_edges[0][i], all_edges[1][i])
-            graph = torch_geometric.data.Data(x=vertices, edge_index=edges)
-            graphs.append(graph)
-        return graphs
+            # Loop over tracksters to get the number of layer clusters
+            n_layer_clusters = len(df.loc[i])
+            X_ID.append(i * np.ones(n_layer_clusters))
+        X_ID = torch.from_numpy(np.concatenate(X_ID))
+        pdb.set_trace()
+        graph = torch_geometric.data.Data(x=vertices, edge_index=event_edges, x_id=X_ID)
+        return graph
 
 
     def get(self, idx):
@@ -98,12 +106,12 @@ class TracksterLoader(Dataset):
         t_get = time.perf_counter();
         file_idx = int((idx % 5000)/5000)
         idx_in_file = idx % 5000
-        edge_index = torch.empty((2,0), dtype=torch.long)
 
         with uproot.open(self.raw_paths[file_idx]) as f:
             tracksters = f["ntuplizer/tracksters"]
             t_pd = time.perf_counter()
             event = ak.to_pandas(tracksters.arrays(entry_start = idx_in_file, entry_stop = idx_in_file + 1)) # Update me!!!! Takes half a second
+            #TODO: This probably needs to be replaced by event.loc[some_index]
             event = event.loc[0] # Now only two indices are left
             data = self.turn_df_to_graph(event)
             return data
@@ -151,27 +159,27 @@ if __name__ == '__main__':
     import os
 
 
-if os.uname()[1] == 'patatrack02.cern.ch':
-    root = "/eos/cms/store/group/dpg_hgcal/comm_hgcal/hackathon/samples/close_by_single_kaon/production/7264977/"
-    regex = "ntuples_7*"
-    N_events = 10000
-elif os.uname()[1] == 'x360':
-    print("Running on x360")
-    root = "/home/philipp/Code/ticl_hackathon_energy_regression/testdata/"
-    regex = 'testdata*'
-    N_events = 10
-else:
-    print("Please specify root path")
+    if os.uname()[1] == 'patatrack02.cern.ch':
+        root = "/eos/cms/store/group/dpg_hgcal/comm_hgcal/hackathon/samples/close_by_single_kaon/production/7264977/"
+        regex = "ntuples_7*"
+        N_events = 10000
+    elif os.uname()[1] == 'x360':
+        print("Running on x360")
+        root = "/home/philipp/Code/ticl_hackathon_energy_regression/testdata/"
+        regex = 'testdata*'
+        N_events = 10
+    else:
+        print("Please specify root path")
 
-dataset = TracksterLoader(root, regex=regex, N_events=N_events)
-print(dataset.len())
+    dataset = TracksterLoader(root, regex=regex, N_events=N_events)
+    print(dataset.len())
 
-train_loader = DataLoader(dataset)
-counter = 0
-for i, data in enumerate(train_loader):
-    if i > 11: break
-    print(f"Event {i}")
-    print(f"Data: {data}")
-    print()
-    print(f"Data[0].x: {data[0].x}")
-    print(f"Data[0].edge_index: {data[0].edge_index}")
+    train_loader = DataLoader(dataset)
+    counter = 0
+    for i, data in enumerate(train_loader):
+        if i > 11: break
+        print(f"Event {i}")
+        print(f"Data: {data}")
+        print()
+        print(f"Data[0].x: {data[0].x}")
+        print(f"Data[0].edge_index: {data[0].edge_index}")
