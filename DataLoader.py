df4591221d3e5aabca5214c1c4af2d3f13f31f55
phzehetn <philipp.zehetner@cern.ch>
2022-06-22 11:16:38 +0200

    merge git_fix

--- DataLoader.py
+++ DataLoader.py
@@ -1,3 +1,5 @@
+import pdb
+import os
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -6,6 +8,7 @@ from torch_geometric.nn.pool import avg_pool_x
 from torch.nn import Sequential, Linear
 import os.path as osp
 from torch_geometric.data import Dataset, download_url, DataLoader, Data
+import torch_geometric
 
 import pandas as pd
 import numpy as np
@@ -13,91 +16,38 @@ import matplotlib.pyplot as plt
 
 import uproot
 import awkward as ak
+import h5py
 
 import glob
 import os.path as osp
 
 import time
 
-class GraphNet(Dataset):
-    r'''
-        input z0: (FatJat basic stats, 1)
-            'fj_pt',
-            'fj_eta',
-            'fj_sdmass',
-            'fj_n_sdsubjets',
-            'fj_doubleb',
-            'fj_tau21',
-            'fj_tau32',
-            'npv',
-            'npfcands',
-            'ntracks',
-            'nsv'
-        input x0: (FatJet info, 1) (also input z1???)
-        'fj_jetNTracks',
-        'fj_nSV',
-        'fj_tau0_trackEtaRel_0',
-        'fj_tau0_trackEtaRel_1',
-        'fj_tau0_trackEtaRel_2',
-        'fj_tau1_trackEtaRel_0',
-        'fj_tau1_trackEtaRel_1',
-        'fj_tau1_trackEtaRel_2',
-        'fj_tau_flightDistance2dSig_0',
-        'fj_tau_flightDistance2dSig_1',
-        'fj_tau_vertexDeltaR_0',
-        'fj_tau_vertexEnergyRatio_0',
-        'fj_tau_vertexEnergyRatio_1',
-        'fj_tau_vertexMass_0',
-        'fj_tau_vertexMass_1',
-        'fj_trackSip2dSigAboveBottom_0',
-        'fj_trackSip2dSigAboveBottom_1',
-        'fj_trackSip2dSigAboveCharm_0',
-        'fj_trackSipdSig_0',
-        'fj_trackSipdSig_0_0',
-        'fj_trackSipdSig_0_1',
-        'fj_trackSipdSig_1',
-        'fj_trackSipdSig_1_0',
-        'fj_trackSipdSig_1_1',
-        'fj_trackSipdSig_2',
-        'fj_trackSipdSig_3',
-        'fj_z_ratio'
-        
-        input x1: (Tracks, max 60)
-        'trackBTag_EtaRel',
-        'trackBTag_PtRatio',
-        'trackBTag_PParRatio',
-        'trackBTag_Sip2dVal',
-        'trackBTag_Sip2dSig',
-        'trackBTag_Sip3dVal',
-        'trackBTag_Sip3dSig',
-        'trackBTag_JetDistVal'
-        
-        input x2: (SVs, max 5)
-        'sv_d3d',
-        'sv_d3dsig',
-        
-        # truth categories are QCD=0 / Hbb=1
-    '''
+class TracksterLoader(Dataset):
 
-    url = '/eos/cms/store/group/dpg_hgcal/comm_hgcal/hackathon/samples/close_by_single_kaon/production/7264977/'
 
-    def __init__(self, root, transform=None):
-        super(GraphNet, self).__init__(root, transform)
+    def __init__(self, root, regex='*.root', N_events=1, transform=None):
+        super(TracksterLoader, self).__init__(root, transform)
         self.strides = [0]
-        # self.calculate_offsets()
         self.root = root
+        self.regex = regex
 
-    def calculate_offsets(self):
-        for path in self.raw_paths:
-            with h5py.File(path, 'r') as f:
-                self.strides.append(f['n'][()][0])
-        self.strides = np.cumsum(self.strides)
-
+    # Maybe should be called __len__
+    # https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
     def len(self):
-        return 5000 * len(root) # is this uniform over all files?
+        # return 5000 * len(self.root) # is this uniform over all files?
+        return 10
 
-    
-    def calculate_edges(df):
+    @property
+    def raw_file_names(self):
+        raw_files = glob.glob(osp.join(self.root, self.regex))
+        return raw_files
+
+    @property
+    def processed_file_names(self):
+        return []
+
+    def calculate_edges(self, df):
         cluster_indices = ['vertices_x', 'vertices_y', 'vertices_z', 'vertices_energy']
         starts = []
         stops = []
@@ -111,52 +61,52 @@ class GraphNet(Dataset):
             for i, z in enumerate(z_values):
                 layer = trackster.where(trackster.vertices_z == z_values[i]).dropna()['subsubentry'].values
                 pre_layer = trackster.where(trackster.vertices_z == z_values[i-1]).dropna()['subsubentry'].values
+                # print(f"layer {i}: layer: {layer}")
+                # print(f"layer {i}: pre_layer: {pre_layer}")
                 for l in layer:
                     for k in layer:
                         if l < k:
-                            trackster_starts.append(l)
-                            trackster_stops.append(k)
+                            trackster_starts.append(l.astype(int))
+                            trackster_stops.append(k.astype(int))
                     for k in pre_layer:
-                        trackster_starts.append(l)
-                        trackster_stops.append(k)
-
+                        trackster_starts.append(l.astype(int))
+                        trackster_stops.append(k.astype(int))
             starts.append(trackster_starts)
             stops.append(trackster_stops)
         return starts, stops
 
 
-    @property
-    def raw_file_names(self):
-        raw_files = glob.glob(osp.join(self.root, 'ntuples_7*'))
-        return raw_files
-
-    @property
-    def processed_file_names(self):
-        return []
+    def turn_df_to_graph(self, df):
+        cluster_indices = ['vertices_x', 'vertices_y', 'vertices_z', 'vertices_energy']
+        graphs = []
+        N_tracksters = df.NTracksters.median().astype(int)
+        all_edges = self.calculate_edges(df)
+        for i in range(N_tracksters):
+            trackster = df.loc[i]
+            vertices = trackster[cluster_indices]
+            vertices = vertices.values
+            vertices = vertices.astype(np.float32)
+            vertices = torch.from_numpy(vertices)
+            edges = (all_edges[0][i], all_edges[1][i])
+            graph = torch_geometric.data.Data(x=vertices, edge_index=edges)
+            graphs.append(graph)
+        return graphs
 
 
     def get(self, idx):
+        cluster_indices = ['vertices_x', 'vertices_y', 'vertices_z', 'vertices_energy']
         t_get = time.perf_counter();
         file_idx = int((idx % 5000)/5000)
         idx_in_file = idx % 5000
-        #if file_idx >= self.strides.size:
-        #    raise Exception(f'{idx} is beyond the end of the event list {self.strides[-1]}')
         edge_index = torch.empty((2,0), dtype=torch.long)
 
         with uproot.open(self.raw_paths[file_idx]) as f:
             tracksters = f["ntuplizer/tracksters"]
             t_pd = time.perf_counter()
-            tmpdf = ak.to_pandas(tracksters.arrays(entry_start = idx_in_file, entry_stop = idx_in_file + 1)) # Update me!!!! Takes half a second
-            t_pd_end = time.perf_counter()
-            tmpdf.index.names = ["Event", "Trackster", "Cluster"]
-            print(t_pd_end-t_pd, time.perf_counter()-t_get)
-
-            # Create node and edge data
-
-            
-
-
-            return Data()
+            event = ak.to_pandas(tracksters.arrays(entry_start = idx_in_file, entry_stop = idx_in_file + 1)) # Update me!!!! Takes half a second
+            event = event.loc[0] # Now only two indices are left
+            data = self.turn_df_to_graph(event)
+            return data
 
             '''
 
@@ -197,20 +147,31 @@ class GraphNet(Dataset):
             '''
 
 
+if __name__ == '__main__':
+    import os
 
-import os
 
-root = "/eos/cms/store/group/dpg_hgcal/comm_hgcal/hackathon/samples/close_by_single_kaon/production/7264977/"
+if os.uname()[1] == 'patatrack02.cern.ch':
+    root = "/eos/cms/store/group/dpg_hgcal/comm_hgcal/hackathon/samples/close_by_single_kaon/production/7264977/"
+    regex = "ntuples_7*"
+    N_events = 10000
+elif os.uname()[1] == 'x360':
+    print("Running on x360")
+    root = "/home/philipp/Code/ticl_hackathon_energy_regression/testdata/"
+    regex = 'testdata*'
+    N_events = 10
+else:
+    print("Please specify root path")
 
-dataset = GraphNet(root)
-
+dataset = TracksterLoader(root, regex=regex, N_events=N_events)
 print(dataset.len())
 
 train_loader = DataLoader(dataset)
 counter = 0
-t = time.perf_counter()
-for data in train_loader:
-    counter = counter +1
-
-
-
+for i, data in enumerate(train_loader):
+    if i > 11: break
+    print(f"Event {i}")
+    print(f"Data: {data}")
+    print()
+    print(f"Data[0].x: {data[0].x}")
+    print(f"Data[0].edge_index: {data[0].edge_index}")
